16616b2ad10155504069599c615097ab5bad2bb7
guilherme_pittner <guilherme_pittner@ethercity.tech>
2023-10-23 22:24:44 -0300

    finished project

--- flask_routes.py
+++ flask_routes.py
@@ -1,17 +1,5 @@
-from flask import Flask, render_template, request, redirect, session, flash
-
-
-class meusJogos():
-    def __init__(self, nome, categoria, plataforma):
-        self.nome = nome
-        self.categoria = categoria
-        self.plataforma = plataforma
-
-
-jogoUm = meusJogos('Sniper Elite', 'Ação', 'PC')
-jogoDois = meusJogos('Final Fantasy', 'RPG', 'PC')
-jogoTres = meusJogos('FIFA', 'Esportes', 'Playstation')
-games = [jogoUm, jogoDois, jogoTres]
+from flask import Flask, render_template, request, redirect, session, flash, url_for
+import classes
 
 
 app = Flask(__name__)
@@ -27,35 +15,40 @@ def mainpage():
     return render_template('login.html', title='Enter with your credentials', next=next)
 
 @app.route('/list')
-def games_list():
+def list():
     if 'current_user' not in session or session['current_user'] == None:
-        return redirect('/?next=list')
-    return render_template('list.html', title='My new title', jogos=games)
+        return redirect(url_for("mainpage", next=url_for("list")))
+    
+    return render_template('list.html', title='My new title', jogos=classes.games)
 
 @app.route('/insert')
-def insertpage():
+def insert():
     if 'current_user' not in session or session['current_user'] == None:
-        return redirect('/?next=insert')
+        return redirect(url_for("mainpage", next=url_for("insert")))
+    
     return render_template('insert.html', title='Insert a new game')
 
 @app.post('/auth')
 def authenticate():
-    if request.form['user_password'] == 'chateau182':
-        session['current_user'] = request.form['username']
-        flash(f"Usuário {session['current_user']} logado com sucesso.")
+    if request.form['username'] in classes.users:
+        actual_user = classes.users[request.form['username']]
 
-        next_page = request.form['next']
-        return redirect(f'/{next_page}')
-    
+        if request.form['user_password'] == actual_user.password:
+            session['current_user'] = actual_user.nickname
+            flash(f"Usuário {actual_user.nickname} logado com sucesso.")
+            
+            next = request.form['next']
+            return redirect(next)
+
     else:
         flash('Algo deu errado... Por favor, revise seus dados e tente novamente.')
-        return redirect('/')
+        return redirect(url_for("mainpage"))
 
 @app.route('/logout')
 def logout():
     session['current_user'] = None
     flash(f"Usuário deslogado.")
-    return redirect('/')
+    return redirect(url_for("mainpage"))
 
 
 #####################
@@ -69,10 +62,10 @@ def new_game():
     categoria = request.form['categoria']
     console = request.form['console']
 
-    novoJogo = meusJogos(nome, categoria, console)
-    games.append(novoJogo)
+    novoJogo = classes.meusJogos(nome, categoria, console)
+    classes.games.append(novoJogo)
 
-    return redirect('/list')
+    return redirect(url_for("games_list"))
 
 
 app.run(debug=True)
